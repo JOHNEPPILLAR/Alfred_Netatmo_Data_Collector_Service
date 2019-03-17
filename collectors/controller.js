c2cd0e06a2157bd155730066c3677999b4ce1554
JP <john.pillar@rbs.co.uk>
2019-03-17 16:45:13 +0000

    Added dev docker

--- collectors/controller.js
+++ collectors/controller.js
@@ -2,15 +2,18 @@
  * Import helper libraries
  */
 const serviceHelper = require('../lib/helper.js');
-const netatmo = require('./netatmo/netatmo.js');
 
 const poolingInterval = 5 * 60 * 1000; // 5 minutes
 
-exports.collectData = async function FnCollectData() {
-  try {
-    await netatmo.getNatemoData(); // Collect Netatmo device data
-  } catch (err) {
-    serviceHelper.log('error', 'Controller - CollectData - Netatmo', err.message);
-  }
-  setTimeout(() => { FnCollectData(); }, poolingInterval); // Wait then run function again
+if (!process.env.Mock) {
+  // eslint-disable-next-line global-require
+  const netatmo = require('./netatmo/netatmo.js');
+  exports.collectData = async function FnCollectData() {
+    try {
+      await netatmo.getNatemoData(); // Collect Netatmo device data
+    } catch (err) {
+      serviceHelper.log('error', 'Controller - CollectData - Netatmo', err.message);
+    }
+    setTimeout(() => { FnCollectData(); }, poolingInterval); // Wait then run function again
+  };
 };
