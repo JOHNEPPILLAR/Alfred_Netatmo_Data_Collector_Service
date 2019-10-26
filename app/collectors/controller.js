1103f6a6644a0993bfa58e5d60eb226b69986c27
JOHNEPPILLAR <johneppillar@gmail.com>
2019-10-26 17:24:10 +0100

    Removed unnecessary mock check

--- app/collectors/controller.js
+++ app/collectors/controller.js
@@ -5,17 +5,15 @@ const serviceHelper = require('alfred-helper');
 
 const poolingInterval = 5 * 60 * 1000; // 5 minutes
 
-if (!process.env.Mock) {
-  // eslint-disable-next-line global-require
-  const netatmo = require('../../app/collectors/netatmo/netatmo.js');
-  exports.collectData = async function FnCollectData() {
-    try {
-      await netatmo.getNatemoData(); // Collect Netatmo device data
-    } catch (err) {
-      serviceHelper.log('error', err.message);
-    }
-    setTimeout(() => {
-      FnCollectData();
-    }, poolingInterval); // Wait then run function again
-  };
-}
+exports.collectData = async function FnCollectData() {
+  try {
+    // eslint-disable-next-line global-require
+    const netatmo = require('../../app/collectors/netatmo/netatmo.js');
+    await netatmo.getNatemoData(); // Collect Netatmo device data
+  } catch (err) {
+    serviceHelper.log('error', err.message);
+  }
+  setTimeout(() => {
+    FnCollectData();
+  }, poolingInterval); // Wait then run function again
+};
