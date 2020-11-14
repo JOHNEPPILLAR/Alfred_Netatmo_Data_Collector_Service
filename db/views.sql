1a000182f98be2b62cf0ad0b4a8d7d8bc12578bb
JP <johneppillar@gmail.com>
2020-11-14 21:02:30 +0000

    Rename name to location in db

--- db/views.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE VIEW vw_battery_data AS
-SELECT last("time", "time") AS "time", last(battery, "time") AS battery, location, 'netatmo' AS device
-FROM netatmo
-WHERE time > NOW() - interval '1 hour' 
-GROUP BY location
